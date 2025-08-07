83c3bc1a9331a0e8091bcbef0f705fed36d71c89
Krzysztof Szklarski <kszklarski7@gmail.com>
2025-08-07 21:40:20 +0200

    added syntax highlighting, markdown to html rendering and autoscroll

--- dist/public/script.js
+++ dist/public/script.js
@@ -1,20 +1,36 @@
+let autoScrollInterval = null;
+
 document.addEventListener('DOMContentLoaded', async () => {
+    hljs.configure({
+        ignoreUnescapedHTML: true
+    });
+
     const chatBlock = document.getElementById("chatBlock");
     const promptForm = document.getElementById('promptForm');
     const promptText = document.getElementById('promptText');
+
+    chatBlock.addEventListener('wheel', () => {
+        if (autoScrollInterval) {
+          stopAutoScroll();
+        };
+    });
 
     promptText.focus();
 
     promptForm.addEventListener('submit', async (event) => {
         event.preventDefault();
-
+        
         const formData = new FormData(promptForm);
         const prompt = formData.get('promptText');
         promptForm.reset();
-
-        chatBlock.innerHTML += `<br> <strong>USER:</strong>`;
-        chatBlock.innerHTML += prompt;
+        
+        const userDiv = document.createElement('div');
+        userDiv.innerHTML += `<br> <strong>USER:</strong><br>`;
+        userDiv.innerHTML += prompt;
+        chatBlock.appendChild(userDiv);
         promptText.focus();
+
+        chatBlock.scrollTop = userDiv.offsetTop;
 
         const response = await fetch("http://localhost:2137/test", {
             method: "POST",
@@ -30,15 +46,51 @@ document.addEventListener('DOMContentLoaded', async () => {
         const decoder = new TextDecoder();
 
         chatBlock.innerHTML += "<br><strong> GPT:</strong>"
+        let markdownBuffer = ""
+        const aiDiv = document.createElement('div');
+        aiDiv.style.marginTop = "0";
+        chatBlock.appendChild(aiDiv);
+
+        startAutoScroll(chatBlock);
 
         while(true){
             const {value, done} = await reader?.read();
-            if (done) break;
+            if (done){
+                stopAutoScroll();
+                break;
+            };
 
             const chunkText = decoder.decode(value, {stream: true})
-            chatBlock.innerHTML += chunkText;
+            const data = handleChunk(chunkText, markdownBuffer);
+            markdownBuffer = data.newBuffer;
+            aiDiv.innerHTML = data.parsed;
+            hljs.highlightAll();
+            console.log(chunkText);
         };
-
     });
 });
+
+function handleChunk(chunk, markdownBuffer){
+    markdownBuffer += chunk;
+    return {
+        "parsed": marked.parse(markdownBuffer),
+        "newBuffer": markdownBuffer
+    };
+};
+
+function startAutoScroll(container) {
+    if (autoScrollInterval !== null) return;
+  
+    autoScrollInterval = setInterval(() => {
+      container.scrollTop += 5;
+    }, 1);
+};
+  
+function stopAutoScroll() {
+    console.error("STOPPED")
+    if (autoScrollInterval) {
+        clearInterval(autoScrollInterval);
+        autoScrollInterval = null;
+    };
+};
 
